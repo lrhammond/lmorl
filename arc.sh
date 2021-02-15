a73c40e5a34a92a08a8b6bbf08c4cdf9342b6d98
Lewis Hammond <lewis@lewishammond.com>
2021-02-15 16:59:40 +0000

    Update installation files

--- arc.sh
+++ arc.sh
@@ -2,16 +2,24 @@
 
 echo "Installing dependencies for ARC..."
 
+# Load required ARC modules
 module purge
 module load anaconda3/2018.12
 module load mujoco/2.00
 
-conda create --prefix $HOME/lmorl/venv --copy python=3.6
+# Create new virtual environment
+conda create --prefix $HOME/lmorl/venv --copy python=3.7
 conda init bash
 conda activate $HOME/lmorl/venv 
 
+# Install packages
 pip install -r requirements.txt
+
+# Note that, apparently, safety-gym requires numpy~=1.17.4 but mujoco-py requires a more recent version
+pip uninstall numpy
+pip install numpy==1.20.1
 
 conda deactivate
 
+# Create extra directories for storing experimental output
 mkdir results logs
