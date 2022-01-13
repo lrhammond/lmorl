e33c526e971600026aef59eeb32a22495f847a54
CJ-Griffin <cj.griffin@protonmail.com>
2022-01-13 21:36:54 +0000

    filename bugfix

--- src/graphing/tf_events_to_txt.py
+++ src/graphing/tf_events_to_txt.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 def convert_tf_to_txt(p):
     assert (os.path.exists(p)), f"path {p} does not exist"
     s_iter = summary_iterator(p)
@@ -12,15 +13,13 @@ def convert_tf_to_txt(p):
         for val in line.summary.value:
             if "Reward" in val.tag: rewards.append(val.simple_value)
             if "Cost" in val.tag: costs.append(val.simple_value)
-    assert(len(rewards) == len(costs))
+    assert (len(rewards) == len(costs))
     mid_name = os.path.split(p)[0].replace("data/", "")
-    print(mid_name, "mid_name")
     file_name = os.path.split(p)[1].replace("events.out.tfevents.", "") + ".txt"
     dir_name = os.path.join("data", "txts", mid_name)
     os.makedirs(dir_name, exist_ok=True)
     txt_p = os.path.join(dir_name, file_name)
     assert (not os.path.exists(txt_p)), f"path {txt_p} already exists"
-    print(txt_p)
     with open(txt_p, 'w') as f:
         for i, (r, c) in enumerate(zip(rewards, costs)):
             f.write('{},{}\n'.format(i, r, c))
