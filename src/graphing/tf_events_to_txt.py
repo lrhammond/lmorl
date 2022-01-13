3fdd01604ad0183eaa10e923eb3c43d25a1bdfb4
CJ-Griffin <cj.griffin@protonmail.com>
2022-01-13 21:28:07 +0000

    undo

--- src/graphing/tf_events_to_txt.py
+++ src/graphing/tf_events_to_txt.py
@@ -14,6 +14,7 @@ def convert_tf_to_txt(p):
             if "Cost" in val.tag: costs.append(val.simple_value)
     assert(len(rewards) == len(costs))
     mid_name = os.path.split(p)[0].replace("data/", "")
+    print(mid_name, "mid_name")
     file_name = os.path.split(p)[1].replace("events.out.tfevents.", "") + ".txt"
     dir_name = os.path.join("data", "txts", mid_name)
     os.makedirs(dir_name, exist_ok=True)
